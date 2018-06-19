820a764a1032dc837a49928de83ba9ad812f39a3
Romain Goncalves <goncalves.romain@gmail.com>
2018-06-19 23:39:27 +0200

    Finding the proper hash based on difficulty

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const sha256 = require('crypto-js/sha256');
 class Blockchain {
   constructor() {
     this.blocks = [];
+    this.difficulty = 1;
   }
 
   addBlock(block) {
     block.previousHash = this.findPreviousHash();
-    this.blocks.push(block);
+    if(this.validateBlock(block)) {
+      this.blocks.push(block);
+    }
+  }
+
+  validateBlock(block) {
+    return true;
   }
 
   findPreviousHash() {
@@ -23,18 +30,46 @@ class Block {
   constructor({data = {}}) {
     this.data = data;
     this.previousHash = '';
+    this.difficulty = 4;
+    this.numberOfZeros = this.generateZeros();
+
     this.hash = this.generateHash();
   }
 
+  generateZeros() {
+    let numberOfZeros = '';
+
+    for (var i = 0; i < this.difficulty; i++) {
+      numberOfZeros = numberOfZeros.concat('0');
+    }
+
+    return numberOfZeros;
+  }
+
   generateHash() {
-    return sha256(JSON.stringify(this.data)).toString();
+    let hash = '';
+    let hashSub = hash.substring(0, this.difficulty);
+    let nounce = 0;
+
+    while (hashSub !== this.numberOfZeros) {
+      hash = sha256(JSON.stringify(this.data + nounce)).toString();
+      hashSub = hash.substring(0, this.difficulty);
+
+      if(hashSub === this.numberOfZeros) {
+        this.nounce = nounce;
+      }
+
+      nounce++;
+    }
+
+    return hash;
   }
 }
 
 const chain = new Blockchain();
 
-for (var i = 0; i < [1,2,3,4].length; i++) {
-  const block = new Block({ data: { amount: [1,2,3,4][i] }});
+for (var i = 0; i < [1].length; i++) {
+  const block = new Block({ data: { amount: [1][i] }});
   chain.addBlock(block);
 }
 
